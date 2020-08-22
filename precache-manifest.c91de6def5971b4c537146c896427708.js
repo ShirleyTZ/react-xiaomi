b19a3777247d3bece5361fc0999de63cf4c55194
ShirleyTZ <553728720@qq.com>
2020-08-22 17:22:58 +0800

    Updates

---
+++ precache-manifest.c91de6def5971b4c537146c896427708.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d1525fd129f3c0261c5",
+    "url": "/reactXiaoMi/static/js/runtime~main.df9daa1c.js"
+  },
+  {
+    "revision": "42a383e4b77769e6db45",
+    "url": "/reactXiaoMi/static/js/main.b74c1c23.chunk.js"
+  },
+  {
+    "revision": "6733a48708c24325b6cb",
+    "url": "/reactXiaoMi/static/js/2.644d897a.chunk.js"
+  },
+  {
+    "revision": "42a383e4b77769e6db45",
+    "url": "/reactXiaoMi/static/css/main.e70e89dc.chunk.css"
+  },
+  {
+    "revision": "34a34f7664bbab8c95822d665ad8edad",
+    "url": "/reactXiaoMi/index.html"
+  }
+];
